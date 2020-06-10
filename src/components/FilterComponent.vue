a4d00d7b0e85a4b3d9fd5eb28f8b6c9d2eae9cca
BrentVanwildemeersch <brent.vanwildemeersch@ocular.be>
2020-06-10 12:05:22 +0200

    Bubble sort functionality

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -2,17 +2,31 @@
 	<div class="w-auto text-white">
 		<div class="text-xl text-center">
 			<button class="p-4 hover:bg-green-800">Retry</button>
-			<button class="p-4 hover:bg-green-800">Play</button>
+			<button class="p-4 hover:bg-green-800" @click="$emit('onClickStartSorting')">Play</button>
 			<button class="p-4 hover:bg-green-800">Stop</button>
 		</div>
 		<div class="text-lg text-center">
 			<div class="inline-block p-2">
 				<label class="relative w-2/12 px-6 text-center">Array Size</label>
-				<input type="range" />
+				<input
+					type="number"
+					class="w-3/12 text-center text-white bg-green-700"
+					:placeholder="400"
+					max="1000"
+					step="1"
+					v-model.number="sizeOfArray"
+				/>
 			</div>
 			<div class="p-2">
 				<label class="relative w-2/12 px-6 text-center">Sorting Speed</label>
-				<input type="range" />
+				<input
+					class="h-3 overflow-hidden bg-gray-400 rounded-lg appearance-none w-128"
+					type="range"
+					min="1"
+					max="100"
+					step="1"
+					value="15"
+				/>
 			</div>
 		</div>
 	</div>
@@ -22,7 +36,23 @@
 	import Vue from "vue";
 
 	export default Vue.extend({
-		name: "FilterComponent"
+		name: "FilterComponent",
+
+		props: {
+			arraySize: { required: true, type: Number, default: 10 }
+		},
+
+		data() {
+			return {
+				sizeOfArray: this.arraySize
+			};
+		},
+
+		watch: {
+			sizeOfArray() {
+				this.$emit("update:arraySize", this.sizeOfArray);
+			}
+		}
 	});
 </script>
 
--- src/views/BubbleSort.vue
+++ src/views/BubbleSort.vue
@@ -1,11 +1,23 @@
 <template>
 	<div class="w-full text-white">
-		<p class="p-4 text-5xl text-center">Bubble Sorting Algorithm</p>
-		<FilterComponent class="fixed bottom-0 w-screen h-auto bg-green-500" />
+		<p class="p-4 text-5xl text-center">Bubble Sort Algorithm</p>
+		<div class="flex inline-block w-full h-auto p-4 sorting_container" v-if="arrItems">
+			<div
+				class="flex-1 float-left bg-green-500 position-relative"
+				v-for="item in arrItems"
+				:key="item.id"
+				:style="item.style"
+			></div>
+		</div>
+		<FilterComponent
+			class="fixed bottom-0 w-screen h-auto bg-green-500"
+			:arraySize.sync="arraySize"
+			@onClickStartSorting="onClickStartBubbleSort"
+		/>
 	</div>
 </template>
 
-<script lang="ts">
+<script >
 	import Vue from "vue";
 	import FilterComponent from "../components/FilterComponent.vue";
 
@@ -13,6 +25,58 @@
 		name: "BubbleSort",
 		components: {
 			FilterComponent
+		},
+		data() {
+			return {
+				arraySize: 10,
+				arrItems: []
+			};
+		},
+
+		mounted() {
+			this.setArrayItems();
+		},
+
+		watch: {
+			arraySize() {
+				this.setArrayItems();
+			}
+		},
+
+		computed: {},
+
+		methods: {
+			setArrayItems() {
+				this.arrItems = [];
+				for (let i = 1; i <= this.arraySize; i++) {
+					const randomNumber = Math.round(
+						Math.random() * (this.arraySize - 1) + 1
+					);
+
+					this.arrItems.push({
+						id: i,
+						value: randomNumber,
+						style: this.getHeightForArrayItem(randomNumber)
+					});
+				}
+			},
+			getHeightForArrayItem(item) {
+				const heightOfElement =
+					window.innerHeight * 0.6 * (item / this.arraySize);
+				return `height:${heightOfElement}px`;
+			},
+			onClickStartBubbleSort() {
+				for (let i = 0; i < this.arraySize; i++) {
+					for (let j = 0; j < this.arraySize - 1 - i; j++) {
+						if (this.arrItems[j].value > this.arrItems[j + 1].value) {
+							const tmp = this.arrItems[j];
+							this.arrItems[j] = this.arrItems[j + 1];
+							this.arrItems[j + 1] = tmp;
+							this.$forceUpdate();
+						}
+					}
+				}
+			}
 		}
 	});
 </script>
