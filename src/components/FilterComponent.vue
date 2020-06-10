739f421051b73076c3a2bed01e91875be8b16899
BrentVanwildemeersch <brent.vanwildemeersch@ocular.be>
2020-06-10 11:13:50 +0200

    Options component

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,5 +1,21 @@
 <template>
-	<div></div>
+	<div class="w-auto text-white">
+		<div class="text-xl text-center">
+			<button class="p-4 hover:bg-green-800">Retry</button>
+			<button class="p-4 hover:bg-green-800">Play</button>
+			<button class="p-4 hover:bg-green-800">Stop</button>
+		</div>
+		<div class="text-lg text-center">
+			<div class="inline-block p-2">
+				<label class="relative w-2/12 px-6 text-center">Array Size</label>
+				<input type="range" />
+			</div>
+			<div class="p-2">
+				<label class="relative w-2/12 px-6 text-center">Sorting Speed</label>
+				<input type="range" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
--- src/views/BubbleSort.vue
+++ src/views/BubbleSort.vue
@@ -1,14 +1,19 @@
 <template>
 	<div class="w-full text-white">
-		<p>BubbleSort</p>
+		<p class="p-4 text-5xl text-center">Bubble Sorting Algorithm</p>
+		<FilterComponent class="fixed bottom-0 w-screen h-auto bg-green-500" />
 	</div>
 </template>
 
 <script lang="ts">
 	import Vue from "vue";
+	import FilterComponent from "../components/FilterComponent.vue";
 
 	export default Vue.extend({
-		name: "BubbleSort"
+		name: "BubbleSort",
+		components: {
+			FilterComponent
+		}
 	});
 </script>
 
